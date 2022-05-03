040895838ae7fbaea1c9261410bc92d060781bc5
Sahil Birman <contact.birmansahil@gmail.com>
2022-05-02 22:48:33 -0400

    learning variables

---
+++ month_03/day_02/index.js
@@ -0,0 +1,11 @@
+//document.getElementById("ppl-count").innerText = 5;
+
+let count = 0;
+
+
+// You can not access a variable before initalizing it, it will result in an error
+let myAge = 35;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
