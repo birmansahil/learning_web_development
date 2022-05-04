3b1c8b7b83592cf8457a11b7f65a5d00138120a3
Sahil Birman <contact.birmansahil@gmail.com>
2022-05-03 22:59:27 -0400

    learning functions and event listeners

---
+++ month_03/day_03/index.js
@@ -0,0 +1,15 @@
+// let bonusPoints = 50;
+
+// bonusPoints = bonusPoints + 50;
+
+// bonusPoints = bonusPoints - 75;
+
+// bonusPoints = bonusPoints + 45;
+
+// console.log(bonusPoints);
+
+// document.getElementById("v-value").innerHTML = bonusPoints;
+
+function increment() {
+    console.log("The button was clicked");
+}
