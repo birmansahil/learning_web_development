3d49361d5ba230187a855425b01451f134e5525c
Sahil Birman <contact.birmansahil@gmail.com>
2022-05-17 23:12:37 -0400

    added points game and predection challenge

--- month_03/practice01/script.js
+++ month_03/practice01/script.js
@@ -1,7 +1,42 @@
 let fullnameEl = document.getElementById("fullname-el");
 let fname = "Mandy";
 let lname = "Yuduski";
+let greetings = "Hi there";
 
 let fullname = fname + " " + lname;
 
 fullnameEl.textContent = fullname;
+
+function greetMandy() {
+    console.log(greetings + ", " + fname + "!");
+}
+
+greetMandy();
+
+// Point game
+
+let myPoints = 3;
+
+function add3Points() {
+    myPoints += 3;
+}
+
+function remove1Point() {
+    myPoints -= 1;
+}
+
+add3Points();
+remove1Point();
+add3Points();
+add3Points();
+remove1Point();
+
+console.log(myPoints);
+
+// Try to predict what each of the lines will log out
+console.log("2" + 2) // 22
+console.log(11 + 7) // 18
+console.log(6 + "5") // 65
+console.log("My points: " + 5 + 9) // My points: 59
+console.log(2 + 2) // 4
+console.log("11" + "14") // 1114
