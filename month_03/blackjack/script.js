178f3e002b68d6a097ffee0bc00d6fd8383c257f
Sahil Birman <contact.birmansahil@gmail.com>
2022-05-24 21:49:41 -0400

    added a new card button

--- month_03/blackjack/script.js
+++ month_03/blackjack/script.js
@@ -1,24 +1,36 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 5;
+let secondCard = 11;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard;
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1];
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
     } else {
-        message = "You're out of the game!"
-        isAlive = false
+        message = "You're out of the game!";
+        isAlive = false;
     }
     messageEl.textContent = message;    
+}
+
+function newCard() {
+    console.log("You want to draw a new card!");
+    let card = 5;
+    sum += card;
+    renderGame();
 }
